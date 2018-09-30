5c7383c8e9960ed3dc5cfdb48e7226a76ce7a500
abhinavkumar311997 <32952715+abhinavkumar311997@users.noreply.github.com>
2018-09-30 10:54:39 +0530

    Add files via upload
    Remaining My-Sql queries

--- part1.php
+++ part1.php
@@ -26,6 +26,18 @@
 		</tr>
 	</table>
 	<br>
+	Count of Employees:<br>
+	<table>
+		<tr>
+			<td>
+				Department:
+			</td>
+			<td>
+				<input type="text" name="dept_name" value="">
+			</td>
+		</tr>
+	</table>
+	<br>
 	Employees Within Department:<br>
 	<table>
 		<tr>
@@ -34,6 +46,38 @@
 			</td>
 			<td>
 				<input type="text" name="tenure" value="">
+			</td>
+		</tr>
+	</table>
+	<br>
+	Gender Ratio:<br>
+	<table>
+		<tr>
+			<td>
+				Department:
+			</td>
+			<td>
+				<input type="text" name="gender" value="">
+			</td>
+		</tr>
+	</table>
+	<br>
+	Gender Pay Ratio:<br>
+	<table>
+		<tr>
+			<td>
+				Department:
+			</td>
+			<td>
+				<input type="text" name="pay" value="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Title:
+			</td>
+			<td>
+				<input type="text" name="title" value="">
 			</td>
 		</tr>
 	</table>
@@ -56,7 +100,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
 	// Create connection
 	// 'About to Connect'."\n";
-	$conn=mysqli_connect("localhost","newuser","@314_Pass","employees");
+	$conn=mysqli_connect($servername,$username,$password,"employees");
 	if (!$conn) {
     		error_log("Failed to connect to database!", 0);
 	}
@@ -66,11 +110,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-	echo 'Connected successfully'."<br>";
+	//echo 'Connected successfully'."<br>";
 	$id=$_POST['id'];
 	$last=$_POST['last'];
 	$dept=$_POST['dept'];
+	$dept_name=$_POST['dept_name'];
 	$tenure=$_POST['tenure'];
+	$gender=$_POST['gender'];
+	$dept_pay=$_POST['pay'];
+	$title=$_POST['title'];
 	if($id!=NULL)
 	{
 		$sql="select * from employees where emp_no=".$id;
@@ -104,6 +152,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 			}
 		}
 	}
+	else if($dept_name!=NULL)
+	{
+		$sql="select * from (select t1.dept_name,count(t2.emp_no) from departments t1  join dept_emp t2 on t1.dept_no=t2.dept_no group by t1.dept_name) as t3 where dept_name='".$dept_name."'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+			// output data of each row
+			while($row = $result->fetch_assoc()) {
+			echo '#'.$row["dept_name"]." ".$row["count(t2.emp_no)"]."<br>";
+			}
+		}
+	}
 	else if($tenure!=NULL)
 	{
 		$sql="SELECT *, DATEDIFF(to_date, from_date) AS tenure, employees.first_name, employees.last_name FROM dept_emp INNER JOIN employees ON employees.emp_no=`dept_emp`.emp_no AND dept_emp.dept_no in (select dept_no from departments where dept_name='".$tenure."') ORDER BY tenure DESC";
@@ -112,6 +171,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 			// output data of each row
 			while($row = $result->fetch_assoc()) {
 			echo '#'.$row["emp_no"].'--'.$row["first_name"].' '.$row["last_name"].'--'.$row["tenure"]."<br>";
+			}
+		}
+	}
+	else if($gender!=NULL)
+	{
+		$sql="select count(emp_no) from employees where gender='M' AND emp_no in (select emp_no from dept_emp where dept_no in (select dept_no from departments where dept_name='".$gender."'))";
+		$result = $conn->query($sql);
+		if($row = $result->fetch_assoc())
+		{
+			$v1=$row['count(emp_no)'];
+			$sql="select count(emp_no) from employees where gender='F' AND emp_no in (select emp_no from dept_emp where dept_no in (select dept_no from departments where dept_name='".$gender."'))";
+			$result = $conn->query($sql);
+			if($row = $result->fetch_assoc())
+			{
+				$v2=$row['count(emp_no)'];
+				if($v2!=0)
+					echo $gender." (Male_To_Female Ratio) : ".($v1/$v2);
+				else
+					echo "No females in ".$gender;
+			}
+		}
+	}
+	else if($dept_pay!=NULL && $title!=NULL)
+	{
+		$sql="select sum(salary) from salaries where emp_no in(select emp_no from employees where emp_no in(select emp_no from titles where title='".$title."' and emp_no in (select emp_no from dept_emp where dept_no in (select dept_no from departments where dept_name='".$dept_pay."'))) and gender='M') and to_date in (select max(to_date) from salaries where emp_no in (select emp_no from titles))";
+		$result = $conn->query($sql);
+		if($row = $result->fetch_assoc())
+		{
+			$v1=$row['sum(salary)'];
+			$sql="select sum(salary) from salaries where emp_no in(select emp_no from employees where emp_no in(select emp_no from titles where title='".$title."' and emp_no in (select emp_no from dept_emp where dept_no in (select dept_no from departments where dept_name='".$dept_pay."'))) and gender='F') and to_date in (select max(to_date) from salaries where emp_no in (select emp_no from titles))";
+			$result = $conn->query($sql);
+			if($row = $result->fetch_assoc())
+			{
+				$v2=$row['sum(salary)'];
+				if($v2!=0)
+					echo $dept_pay." (Male_To_Female Pay for ".$title." ) : ".($v1/$v2);
+				else
+					echo "No females in ".$dept_pay." working as ".$title;
 			}
 		}
 	}
